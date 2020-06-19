d648ccde0eebc012e4378e772aef2f850bc14f35
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-19 09:45:20 -0700

    Add Order and Pizza constructor functions to business logic of scripts.js, call first instance of each in user interface logic of scripts.js with placeholders for submit function.

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+//BUSINESS LOGIC
+
+//business logic to start a new customer order
+function Order() {
+  this.pizzas = [];
+  this.pizzaIdNumber = 0;
+}
+
+
+
+//business logic to make a pizza for the order
+function Pizza(size, crust, sauce, cheese, toppings) {
+  this.size = size;
+  this.crust = crust;
+  this.sauce = sauce;
+  this.cheese = cheese;
+  this.toppings = [];
+}
+
+
+
+
+
+
+
+
+//USER INTERFACE LOGIC
+$(document).ready(function(){
+  let customerOrder = new Order();
+  // $(pizzaform).submit(function(event) {
+  //   event.preventDefault();
+    let myPizza = new Pizza("small", "thin", "marinara", "mozzarella");
+  // });
+});
